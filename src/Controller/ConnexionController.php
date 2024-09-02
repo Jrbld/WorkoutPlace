157ae62ee75198f2a57952749013b02e071f81ff
Jérémy Ballaud <jeremy.ballaud.dev@gmail.com>
2024-09-02 13:26:22 +0200

    Navbar & footer responsives for desktop & tablet

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConnexionController extends AbstractController
 {
-    #[Route('/connexion', name: 'connexion')]
+    #[Route('/connexion', name: 'connexion', methods: ['GET', 'POST'])]
 
-    public function index(Request $request): Response
+    public function login(): Response
     {
         return $this->render('connexion/connexion.twig');
+    }
+
+    #[Route('/deconnexion', name: 'deconnexion')]
+
+    public function logout()
+    {
+        return $this->render('connexion/deconnexion.twig');
     }
 }
