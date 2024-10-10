070a12b4a87c7607c292b0322593daf80b3876be
Jérémy Ballaud <jeremy.ballaud.dev@gmail.com>
2024-10-10 20:38:56 +0200

    Choose subscription page in works and add buttons on Account & Subscription pages

--- src/Controller/ChooseSubscriptionController.php
+++ src/Controller/ChooseSubscriptionController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Subscription;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
 
 class ChooseSubscriptionController extends AbstractController
 {
